d477c7d4cc58e38e75e7af14ae767fd0e70ca4a6
kayvan61 <kayvan.mansoor@gmail.com>
2020-11-06 01:29:49 -0600

    Added supporting files for the bokeh self check.

---
+++ python_util/randomBar.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+from bokeh.embed import components
+from bokeh.models import ColumnDataSource, HoverTool, PrintfTickFormatter
+from bokeh.plotting import figure
+from bokeh.transform import factor_cmap
+
+
+def get_bokeh_chart():
+	# Creating a list of categories
+	years = np.arange(15)
+	values = np.arange(15)
+	
+	p = figure( plot_height=300, 
+				plot_width=400,
+				title="Measles in the USA 2000-2015")#Plotting
+	p.vbar(years,                            #categories
+		  top = values,                      #bar heights
+		   width = .9,
+		   fill_alpha = .5,
+		   fill_color = 'salmon',
+		   line_alpha = .5,
+		   line_color='green',
+		   line_dash='dashed'
+		  
+	  )#Signing the axis
+	p.xaxis.axis_label="Years"
+	p.yaxis.axis_label="Measles stats"
+	
+	return components(p)
